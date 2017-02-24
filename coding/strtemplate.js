7df70101c6b763d4b3cd4a578b658a0dc9e7d8a4
wxb <wxb0328@icloud.com>
2017-02-24 10:51:14 +0800

    express

---
+++ coding/strtemplate.js
@@ -0,0 +1,29 @@
+
+"use strict"
+
+var a = 1;
+var b = 2;
+
+var res = `RESULT: ${a} + ${b} = ${a+b}`
+
+console.info(res)
+
+let name = 'wangxb';
+
+var hello = `hello, ${'world'} and ${name}`;
+console.info(hello);
+
+var bb = `hi, ${'wangxb'}`;
+console.info(bb);
+
+var c
+c
+=
+1
+console.info(c);
+
+var x,f
+
+var y = x+f
+;(a+b).toString()
+
